67ea203b736179cc2e50bc952ebabaa6f115649d
syed-growexxer <114057852+syed-growexxer@users.noreply.github.com>
2022-12-07 17:38:17 +0530

    New Commit

---
+++ android/app/src/main/kotlin/com/example/nft2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nft2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ethereum_example.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:convert/convert.dart';
+import 'package:flutter/material.dart';
+import 'package:trust_wallet_core/flutter_trust_wallet_core.dart';
+import 'package:trust_wallet_core/trust_wallet_core_ffi.dart';
+import 'package:nft2/base_example.dart';
+import 'package:trust_wallet_core/protobuf/Ethereum.pb.dart' as Ethereum;
+
+class EthereumExample extends BaseExample {
+  final HDWallet wallet;
+
+  const EthereumExample(this.wallet, {Key? key}) : super('Ethereum', key: key);
+
+  @override
+  _EthereumExampleState createState() => _EthereumExampleState();
+}
+
+class _EthereumExampleState extends BaseExampleState<EthereumExample> {
+  @override
+  void initState() {
+    super.initState();
+    logger.d("mnemonic: ${widget.wallet.mnemonic()}");
+    logger.d("address: ${widget.wallet.getAddressForCoin(TWCoinType.TWCoinTypeEthereum)}");
+    final publicKey = widget.wallet.getKeyForCoin(TWCoinType.TWCoinTypeEthereum).getPublicKeySecp256k1(false);
+    AnyAddress anyAddress = AnyAddress.createWithPublicKey(publicKey, TWCoinType.TWCoinTypeEthereum);
+    logger.d("address from any address: ${anyAddress.description()}");
+    print(widget.wallet.mnemonic());
+    String privateKeyhex = hex.encode(widget.wallet.getKeyForCoin(TWCoinType.TWCoinTypeEthereum).data());
+    logger.d("privateKeyhex: $privateKeyhex");
+    logger.d("seed = ${hex.encode(widget.wallet.seed())}");
+    final keystore = StoredKey.importPrivateKey(widget.wallet.getKeyForCoin(TWCoinType.TWCoinTypeEthereum).data(), "name", "password", TWCoinType.TWCoinTypeEthereum);
+    logger.d("keystore: ${keystore?.exportJson()}");
+
+  }
+}
